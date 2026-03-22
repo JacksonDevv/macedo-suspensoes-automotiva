44dc5c32440d15d691764a640a4b18b37f595826
Jacksondevv <cotchoco@hotmail.com>
2026-03-22 17:38:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0) scale(1)";
+    }
+  });
+}, { threshold: 0.15 });
+
+elements.forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(80px) scale(0.95)";
+  el.style.transition = "all 0.8s ease";
+  observer.observe(el);
+});
